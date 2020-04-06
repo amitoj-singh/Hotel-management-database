cecb8c5f3ff13aededed77c24644b26efb3e9a04
Amitoj Singh <amitoj.98@gmail.com>
2020-04-06 21:40:37 +0530

    display vacant rooms, their category and cost in a particular hotel

---
+++ queries/vacant rooms.sql
@@ -0,0 +1,8 @@
+-- Display the number of rooms vacant of all the room categories and their cost in a particular hotel
+SELECT  room_categories.name AS category, room_categories.description, room_categories.cost, count(*) AS num_of_rooms
+FROM room_categories
+INNER JOIN rooms
+ON room_categories.room_category_id = rooms.room_room_category_id
+WHERE hotel_hotel_id = 1 and booking_booking_id is NULL
+GROUP BY room_categories.name, room_categories.room_category_id
+ORDER BY room_categories.room_category_id;
